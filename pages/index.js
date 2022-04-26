355c47263a6cc1b05d526e637c7687bef3250882
Dogu Yilmaz <douscriptist@gmail.com>
2022-04-26 14:24:52 +0300

    mini responsive

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Box, Container, Heading, Image, Text } from '@chakra-ui/react';
+import { Box, Container, Flex, Heading, Image, Text, VStack } from '@chakra-ui/react';
 import Head from 'next/head';
 
 export default function Home() {
@@ -16,50 +16,52 @@ export default function Home() {
 
       <Box
         h='full'
-        pt='15rem'
-        marginLeft={['3rem', '5rem', '5rem', '6rem', '10rem']}
+        pt={['4rem', '15rem']}
+        marginLeft={['1rem', '5rem', '5rem', '6rem', '10rem']}
         backgroundImage='url("/images/layer.png")'
         backgroundRepeat='no-repeat'
         backgroundPosition={['10rem', '8rem', '15rem', '25rem', '30rem', '50rem']}
         backgroundSize='contain'
       >
-        <Image objectFit='cover' src='/images/buray.png' alt='Buray Yüksel' />
+        <Box>
+          <Image objectFit='cover' src='/images/buray.png' alt='Buray Yüksel' />
 
-        <Heading
-          fontSize={['30px', '40px', '45px', '55px', '65px', '91px']}
-          lineHeight='91px'
-          mt={[1, 3, 5]}
-          fontFamily='Poppins'
-          fontWeight={275}
-          color='#FFFFFF'
-        >
-          Buray Yüksel
-        </Heading>
-        <Heading
-          fontWeight={400}
-          fontSize={[50, 60, 70, 80, 90, 140]}
-          lineHeight={['50px', '50px', '50px', '60px', '65px', '91px']}
-          mt={[1, 1, 2, 2, 5]}
-          fontFamily='GopherDisplay'
-          color='#FFFFFF'
-        >
-          Product Designer
-        </Heading>
+          <Heading
+            fontSize={['20px', '40px', '45px', '55px', '65px', '91px']}
+            lineHeight={['50px', '91px']}
+            mt={[1, 2, 3, 5]}
+            fontFamily='Poppins'
+            fontWeight={275}
+            color='#FFFFFF'
+          >
+            Buray Yüksel
+          </Heading>
+          <Heading
+            fontWeight={400}
+            fontSize={[30, 60, 70, 80, 90, 140]}
+            lineHeight={['10px', '50px', '50px', '60px', '65px', '91px']}
+            mt={[0, 1, 2, 2, 5]}
+            fontFamily='GopherDisplay'
+            color='#FFFFFF'
+          >
+            Product Designer
+          </Heading>
 
-        <Text
-          fontSize={['16px', '18px']}
-          lineHeight={['25px', '27px']}
-          mt={[10, 30, 50]}
-          fontFamily='Poppins'
-          fontWeight={275}
-          color='#FFFFFF'
-          w={['360px', '380px', '620px', '864px']}
-        >
-          I have 7 years experience as a designer in UI Designer and Art Director positions. Currently, I work as an Product Designer at Türkiye
-          Tourism Promotion and Development Agency, designing user interface and user experience for product across all major platforms - iOS, OS X,
-          Android by using Adobe Illustrator, Adobe Photoshop, Sketch App, Figma, inVision and other UX applications, creating assets, icons and
-          illustrations, besides this creating wireframes, work flows, prototypes and interactions.
-        </Text>
+          <Text
+            fontSize={['16px', '18px']}
+            lineHeight={['25px', '27px']}
+            mt={[10, 30, 50]}
+            fontFamily='Poppins'
+            fontWeight={275}
+            color='#FFFFFF'
+            w={['340px', '380px', '620px', '864px']}
+          >
+            I have 7 years experience as a designer in UI Designer and Art Director positions. Currently, I work as an Product Designer at Türkiye
+            Tourism Promotion and Development Agency, designing user interface and user experience for product across all major platforms - iOS, OS X,
+            Android by using Adobe Illustrator, Adobe Photoshop, Sketch App, Figma, inVision and other UX applications, creating assets, icons and
+            illustrations, besides this creating wireframes, work flows, prototypes and interactions.
+          </Text>
+        </Box>
       </Box>
     </Box>
   );
